d13606f2da131f5d11d2420e50dc5157ceccf4ed
wooilkyu <wooilkyu@naver.com>
2021-06-01 22:48:25 +0900

    project2

--- rpsGame/main.swift
+++ rpsGame/main.swift
@@ -7,18 +7,22 @@
 
 import Foundation
 
-var randomValue: Int = createRandomValue()
-var userNum: Int = inputUserValue()
+var randomValue: Int = 0
 var isPlay = true
 
 func startGame() {
     repeat {
-        print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
+        randomValue = createRandomValue()
         print(randomValue)
-        if userNum < 4 && userNum > 0 {
-            compare()
-        }
+        print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
+        let userNum = inputUserValue()
+        compare(userNum: userNum)
     } while isPlay == true
+}
+
+func exitGame() {
+    print("게임 종료")
+    isPlay = false
 }
 
 func createRandomValue() -> Int {
@@ -32,54 +36,42 @@ func inputUserValue() -> Int {
     return userValue
 }
 
-func compare() {
+func compare(userNum: Int) {
+    print(randomValue)
     switch userNum {
     case 0:
         exitGame()
     case 1:
         if randomValue == 3 {
             print("이겼습니다!")
-            exitGame()
         } else if randomValue == 2 {
             print("졌습니다!")
-            exitGame()
         } else if randomValue == 1 {
             print("비겼습니다!")
         }
     case 2:
         if randomValue == 3 {
             print("졌습니다!")
-            exitGame()
         } else if randomValue == 2 {
             print("비겼습니다!")
         } else if randomValue == 1 {
             print("이겼습니다!")
-            exitGame()
         }
     case 3:
         if randomValue == 3 {
             print("비겼습니다!")
         } else if randomValue == 2 {
             print("이겼습니다!")
-            exitGame()
         } else if randomValue == 1 {
             print("졌습니다!")
-            exitGame()
         }
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        startGame()
     }
 }
 
 func printResult() {
-    
-}
 
-func exitGame() {
-    print("게임 종료")
-    isPlay = false
 }
 
 startGame()
-
