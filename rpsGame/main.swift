ca336ba1ef839d094d7e300b4265e506a201c58d
DONG-WOON <woon4910@gmail.com>
2021-06-01 18:48:16 +0900

    fix : Make var and func

--- rpsGame/main.swift
+++ rpsGame/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: - STEP1. 변수 함수 생성
+//변수
 
+var randomValue: Int = createRandomValue()
+
+
+
+//함수
+func startGame() {
+    
+}
+
+func createRandomValue() -> Int {
+    let computedValue = Int.random(in: 1...3)
+    return computedValue
+}
+
+func inputUserValue() {
+    
+}
+
+func printResult() {
+    
+}
+
+func exitGame() {
+    print("게임 종료")
+}
+
+
+print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
+print(randomValue)
